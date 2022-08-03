ef75f2c1fe9009075149cf20d7368ea90727972d
prakashkancham <96066599+prakashkancham@users.noreply.github.com>
2022-08-03 12:10:44 +0530

    Update ConnectionDao.java

--- src/main/java/com/websystique/springmvc/dao/ConnectionDao.java
+++ src/main/java/com/websystique/springmvc/dao/ConnectionDao.java
@@ -15,7 +15,7 @@ public class ConnectionDao {
 	try {
 		Class.forName("com.mysql.jdbc.Driver");  
 		con=DriverManager.getConnection(  
-		"jdbc:mysql://awsrds.cuaofirbrh2o.us-west-2.rds.amazonaws.com:3306/user","username","password");
+		"jdbc:mysql://awsrds.cvfepqq1qjag.ap-south-1.rds.amazonaws.com:3306/user", "awsrds", "awsrdsmysql");
 		}catch(Exception e){ System.out.println(e);}
 	return con;  
 	}
